a123aea6b0547a345db024e6c588378d7fe7f7c6
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-09-28 10:50:20 -0500

    first commit

---
+++ script copy.js
@@ -0,0 +1,10 @@
+var myFriends =["Eric", "Daniel", "Brandon", "Tyffani", "Erica"];
+
+for (let i=0; i<myFriends.length; i++){
+    console.log(myFriends [i], ':');
+    for (n=99; n>1; n--) {
+        console.log(n + 'lines of code in the file'+ n +'lines of code;'+ myFriends[i]+ 'strikes one out, clears it all out' + (n-1) + 'lines of code in the file');}
+    if (n===1);{
+        console.log(n + 'line of code in the file'+ n + 'line of code;' + myFriends[i]+ 'strikes one out. clears it all out, no more lines of code in the file');
+    }
+}
